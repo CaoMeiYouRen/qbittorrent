52dc0e4568d31310b9143265b593b31d0668753e
CaoMeiYouRen <996881204@qq.com>
2024-04-02 14:51:58 +0800

    feat: add CJS support

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  minify: false,
+  noExternal: [/(.*)/],
+})
